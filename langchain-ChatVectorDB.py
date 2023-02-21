1e00ce35d58a84660439e6e74e08e97f1971baf2
Connor Shorten <connorshorten300@gmail.com>
2023-02-21 16:37:37 +0100

    Create langchain-ChatVectorDB.py

---
+++ langchain-ChatVectorDB.py
@@ -0,0 +1,25 @@
+from langchain.vectorstores.weaviate import Weaviate
+from langchain.llms import OpenAI
+from langchain.chains import ChatVectorDBChain
+import weaviate
+
+client = weaviate.Client("http://localhost:8080")
+
+vectorstore = Weaviate(client, "PodClip", "content")
+# ^ Better explain the Weavaite Class - Property here
+
+MyOpenAI = OpenAI(temperature=0.2, 
+    openai_api_key="ENTER YOUR OPENAI KEY HERE")
+
+qa = ChatVectorDBChain.from_llm(MyOpenAI, vectorstore)
+
+chat_history = []
+
+print("Welcome to the Weaviate ChatVectorDBChain Demo!")
+print("Please enter a question or dialogue to get started!")
+
+while True:
+    query = input("")
+    result = qa({"question": query, "chat_history": chat_history})
+    print(result["answer"])
+    chat_history = [(query, result["answer"])]
